75c1b19e1e00f3b46460e3a14fc6fce2a7ed893e
44Kwon <kwh3217@gmail.com>
2025-08-23 18:39:41 +0900

    유저 검증과 관련된 클래스

---
+++ src/main/java/com/gamemarket/user/service/UserValidator.java
@@ -0,0 +1,49 @@
+package com.gamemarket.user.service;
+
+import com.gamemarket.exception.BusinessLogicException;
+import com.gamemarket.exception.ExceptionCode;
+import com.gamemarket.user.entity.User;
+import com.gamemarket.user.repository.UserRepository;
+import com.gamemarket.user.service.dto.UserServiceDto;
+import org.springframework.stereotype.Component;
+
+/**
+ * 입력값에 대한 검증 로직을 담당하는 클래스
+ */
+@Component
+public class UserValidator {
+
+    private final UserRepository userRepository;
+
+    public UserValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void validate(User user) {
+        validateUsername(user.getUsername());
+        validateEmail(user.getEmail());
+    }
+
+    public void validateUsername(String username) {
+        if (userRepository.existsUserByUsername(username)) {
+            throw new BusinessLogicException(ExceptionCode.USER_USERNAME_DUPLICATED);
+        }
+    }
+
+    public void validateEmail(String email) {
+        if (userRepository.existsUserByUsername(email)) {
+            throw new BusinessLogicException(ExceptionCode.USER_EMAIL_DUPLICATED);
+        }
+    }
+
+    public void validateNewPassword(UserServiceDto.updatePassword updatePasswordDto) {
+        if (updatePasswordDto.getCurrentPw().equals(updatePasswordDto.getNewPw())) {
+            throw new BusinessLogicException(ExceptionCode.PASSWORD_SAME_AS_OLD);
+        }
+    }
+
+    // userId로 유저 존재 여부 검증
+    public User findVerifiedUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+    }
+}
