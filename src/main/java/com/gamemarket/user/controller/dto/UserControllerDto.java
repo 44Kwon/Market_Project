40b71833dfb37016436d5dee084f009d8a89667d
44Kwon <kwh3217@gmail.com>
2025-08-23 18:38:52 +0900

    controller 레이어 Dto

---
+++ src/main/java/com/gamemarket/user/controller/dto/UserControllerDto.java
@@ -0,0 +1,34 @@
+package com.gamemarket.user.controller.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserControllerDto {
+
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = false) // => 허용되지 않은 값이 추가로 들어오면 튕겨내기
+    public static class create {
+        @NotBlank(message = "아이디를 입력해주세요.")
+        private String username;
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        @NotBlank(message = "이메일을 입력해주세요.")
+        private String email;
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    public static class updatePassword {
+        @NotBlank(message = "기존 비밀번호를 입력해주세요.")
+        private String currentPw;
+        @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
+        private String newPw;
+    }
+}
