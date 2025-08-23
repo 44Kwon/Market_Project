cacbbff5cae0b238fd36d4c591f0d232edb3ff6b
44Kwon <kwh3217@gmail.com>
2025-08-23 18:39:13 +0900

    JPA Repository

---
+++ src/main/java/com/gamemarket/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gamemarket.user.repository;
+
+import com.gamemarket.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsUserByUsername(String username);
+}
